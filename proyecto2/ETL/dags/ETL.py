8d13d4bcdec5b5a6bc79c464d5150566417d6858
sepenuela <se.penuela@uniandes.edu.co>
2022-12-05 23:12:26 -0500

    Implementacion ETL

--- proyecto2/ETL/dags/ETL.py
+++ proyecto2/ETL/dags/ETL.py
@@ -39,6 +39,13 @@ with DAG(
     sql= crear_tablas()
     )
 
+    # task: 2.9 poblar tabla municipio
+    poblar_municipio = PostgresOperator(
+        task_id="poblar_municipio",
+        postgres_conn_id='postgres_localhost',
+        sql=insert_query_municipio(csv_path = "dimension_municipio")
+    )
+
     # task: 3 poblar las tablas de dimensiones en la base de datos
     with TaskGroup('poblar_tablas') as poblar_tablas_dimensiones:
 
@@ -46,56 +53,56 @@ with DAG(
         poblar_city = PostgresOperator(
             task_id="poblar_municipio",
             postgres_conn_id='postgres_localhost',
-            sql=insert_query_municipio(csv_path = "dimension_city")
+            sql=insert_query_municipio(csv_path = "dimension_municipio")
         )
 
         # task: 3.2 poblar tabla vivienda
         poblar_customer = PostgresOperator(
             task_id="poblar_vivienda",
             postgres_conn_id='postgres_localhost',
-            sql=insert_query_fact_CoberturaAcueducto_vivienda(csv_path ="dimension_customer")
+            sql=insert_query_fact_CoberturaAcueducto_vivienda(csv_path ="dimension_vivienda")
         )
 
         # task: 3.3 poblar tabla conflicto
         poblar_date = PostgresOperator(
             task_id="poblar_conflicto",
             postgres_conn_id='postgres_localhost',
-            sql=insert_query_conflicto(csv_path = "dimension_date")
+            sql=insert_query_conflicto(csv_path = "dimension_conflicto")
         )
 
         # task: 3.4 poblar tabla demografia
         poblar_employee = PostgresOperator(
             task_id="poblar_demografia",
             postgres_conn_id='postgres_localhost',
-            sql=insert_query_demografia(csv_path = "dimension_employee")
+            sql=insert_query_demografia(csv_path = "dimension_demografia")
         )
 
         # task: 3.5 poblar tabla desempleo
         poblar_stock_item = PostgresOperator(
             task_id="poblar_desempleo",
             postgres_conn_id='postgres_localhost',
-            sql=insert_query_desempleo(csv_path = "dimension_stock_item")
+            sql=insert_query_desempleo(csv_path = "dimension_desempleo")
         )
 
         # task: 3.6 poblar tabla educacion
         poblar_stock_item = PostgresOperator(
             task_id="poblar_educacion",
             postgres_conn_id='postgres_localhost',
-            sql=insert_query_fact_educacion(csv_path = "dimension_stock_item")
+            sql=insert_query_fact_educacion(csv_path = "dimension_educacion")
         )
 
         # task: 3.7 poblar tabla salud
         poblar_stock_item = PostgresOperator(
             task_id="poblar_salud",
             postgres_conn_id='postgres_localhost',
-            sql=insert_query_fact_salud(csv_path = "dimension_stock_item")
+            sql=insert_query_fact_salud(csv_path = "dimension_salud")
         )
 
         # task: 3.8 poblar tabla pobreza
         poblar_stock_item = PostgresOperator(
             task_id="poblar_pobreza",
             postgres_conn_id='postgres_localhost',
-            sql=insert_query_fact_pobreza(csv_path = "dimension_stock_item")
+            sql=insert_query_fact_pobreza(csv_path = "dimension_pobreza")
         )
 
 
@@ -103,8 +110,8 @@ with DAG(
     poblar_fact_order = PostgresOperator(
             task_id="construir_tabla_de_hechos_mineria",
             postgres_conn_id='postgres_localhost',
-            sql=insert_query_mineria(csv_path = "fact_order")
+            sql=insert_query_mineria(csv_path = "hecho_mineria")
     )
 
     # flujo de ejecución de las tareas  
-    preprocesamiento >> crear_tablas_db >> poblar_tablas_dimensiones >> poblar_fact_order
+    preprocesamiento >> crear_tablas_db >> poblar_municipio >> poblar_tablas_dimensiones >> poblar_fact_order
--- proyecto2/ETL/dags/utils/crear_tablas.py
+++ proyecto2/ETL/dags/utils/crear_tablas.py
@@ -9,72 +9,96 @@ def crear_tablas():
 
         CREATE TABLE IF NOT EXISTS hecho_produccion_minera(
             HPM_PKDWH SERIAL PRIMARY KEY,
-            Codigo_entidad DECIMAL FOREIGN KEY REFERENCES municipio (Codigo_entidad),
+            Codigo_entidad DECIMAL,
             Recurso_natural VARCHAR(150),
             Año DECIMAL,
             Trimestre VARCHAR(150),
-            Valor_contraprestacion DECIMAL
+            Valor_contraprestacion DECIMAL,
+            CONSTRAINT FK_Codigo_entidad
+                FOREIGN KEY(Codigo_entidad)
+                    REFERENCES municipio(Codigo_entidad)
         );
 
         CREATE TABLE IF NOT EXISTS numPersonasDesplazadas_Conflicto(
             NPD_PKDWH SERIAL PRIMARY KEY,
-            Codigo_entidad DECIMAL FOREIGN KEY REFERENCES municipio (Codigo_entidad),
+            Codigo_entidad DECIMAL,
             DatoNumerico DECIMAL,
             Año DECIMAL,
-            Mes DECIMAL
+            Mes DECIMAL,
+            CONSTRAINT FK_Codigo_entidad
+                FOREIGN KEY(Codigo_entidad)
+                    REFERENCES municipio(Codigo_entidad)
         );
 
         CREATE TABLE IF NOT EXISTS PuntajeSisben_Demografia(
             PSD_PKDWH SERIAL PRIMARY KEY,
-            Codigo_entidad DECIMAL FOREIGN KEY REFERENCES municipio (Codigo_entidad),
+            Codigo_entidad DECIMAL,
             DatoNumerico DECIMAL,
             Año DECIMAL,
-            Mes DECIMAL
+            Mes DECIMAL,
+            CONSTRAINT FK_Codigo_entidad
+                FOREIGN KEY(Codigo_entidad)
+                    REFERENCES municipio(Codigo_entidad)
         );
 
 
         CREATE TABLE IF NOT EXISTS MDM_desempleo(
             MDM_PKDWH SERIAL PRIMARY KEY,
-            Codigo_entidad DECIMAL FOREIGN KEY REFERENCES municipio (Codigo_entidad),
+            Codigo_entidad DECIMAL,
             DatoNumerico DECIMAL,
             Año DECIMAL,
-            Mes DECIMAL
+            Mes DECIMAL,
+            CONSTRAINT FK_Codigo_entidad
+                FOREIGN KEY(Codigo_entidad)
+                    REFERENCES municipio(Codigo_entidad)
         );
 
 
          CREATE TABLE IF NOT EXISTS CoberturaAcueducto_vivienda(
             CAV_PKDWH SERIAL PRIMARY KEY,
-            Codigo_entidad DECIMAL FOREIGN KEY REFERENCES municipio (Codigo_entidad),
+            Codigo_entidad DECIMAL,
             DatoNumerico DECIMAL,
             Año DECIMAL,
-            Mes DECIMAL
+            Mes DECIMAL,
+            CONSTRAINT FK_Codigo_entidad
+                FOREIGN KEY(Codigo_entidad)
+                    REFERENCES municipio(Codigo_entidad)
         );
 
 
          CREATE TABLE IF NOT EXISTS PuntajePromedioICFES_educacion(
             PSD_PKDWH SERIAL PRIMARY KEY,
-            Codigo_entidad DECIMAL FOREIGN KEY REFERENCES municipio (Codigo_entidad),
+            Codigo_entidad DECIMAL,
             Indicador VARCHAR(250),
             DatoNumerico DECIMAL,
             Año DECIMAL,
-            Mes DECIMAL
+            Mes DECIMAL,
+            CONSTRAINT FK_Codigo_entidad
+                FOREIGN KEY(Codigo_entidad)
+                    REFERENCES municipio(Codigo_entidad)
         );
 
 
          CREATE TABLE IF NOT EXISTS MortalidadNeonatal_salud(
             MNS_PKDWH SERIAL PRIMARY KEY,
-            Codigo_entidad DECIMAL FOREIGN KEY REFERENCES municipio (Codigo_entidad),
+            Codigo_entidad DECIMAL,
             DatoNumerico DECIMAL,
             Año DECIMAL,
-            Mes DECIMAL
+            Mes DECIMAL,
+            CONSTRAINT FK_Codigo_entidad
+                FOREIGN KEY(Codigo_entidad)
+                    REFERENCES municipio(Codigo_entidad)
         );
 
 
          CREATE TABLE IF NOT EXISTS IPM_pobreza(
             IPM_PKDWH SERIAL PRIMARY KEY,
-            Codigo_entidad DECIMAL FOREIGN KEY REFERENCES municipio (Codigo_entidad),
+            Codigo_entidad DECIMAL,
             DatoNumerico DECIMAL,
             Año DECIMAL,
-            Mes DECIMAL
+            Mes DECIMAL,
+            CONSTRAINT FK_Codigo_entidad
+                FOREIGN KEY(Codigo_entidad)
+                    REFERENCES municipio(Codigo_entidad)
         );
     """
